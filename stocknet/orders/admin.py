983f9f784793bdb9aec127099e4809214302ff72
jayliam <51280073+jayliam@users.noreply.github.com>
2020-04-03 01:09:13 +0200

    order

--- stocknet/orders/admin.py
+++ stocknet/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ClientOrder
+from .models import SupplierOrder
 
-# Register your models here.
+
+admin.site.register(ClientOrder)
+admin.site.register(SupplierOrder)
--- stocknet/stocknet/settings.py
+++ stocknet/stocknet/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'clients',
+    'orders',
     'suppliers',
     'phone_field',
     'multiselectfield',
