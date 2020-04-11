44a78241c3c3b6890a1c1f857c07c97e424e32a3
jayliam <51280073+jayliam@users.noreply.github.com>
2020-04-11 23:08:44 +0200

    pushs

--- stocknet/stocknet/settings.py
+++ stocknet/stocknet/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'stocknet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "PS",
+        'NAME': "PS2",
         'USER': 'postgres',
         'PASSWORD' : '1234',
         'HOST' : 'localhost'
